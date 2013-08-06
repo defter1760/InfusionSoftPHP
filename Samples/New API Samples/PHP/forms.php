58359a632abfe9510da18e99497ea0749e8aec19
defter1760 <defter@gmail.com>
2013-08-06 11:03:02 -0700

    cleaned up table structure and updated fields
    cleaned up tables with proper indents for easier reading and added
    inline field population for form data, updated posttovars.php with all
    contact info fields

--- Samples/New API Samples/PHP/forms.php
+++ Samples/New API Samples/PHP/forms.php
@@ -35,88 +35,271 @@ require('posttovars.php');
     </tr>    
 <form action="forms.php" method="POST" class="form">
 
-    <tr><td>Unique ID</td><td ><input type="text" name="ID" disabled
-    <?PHP
-	if(!isset($conID))
-	{
-	    if(isset($ID))
-	    {
-		echo 'value='.$ID;
-	    }
-	}
-	else
-	{
-	    echo 'value='.$conID;
-	}
-    ?> /></td></tr>
-<tr><td>First Name</td><td><input type="text" name="fName" /></td></tr>
-<tr><td>Last Name</td><td><input type="text" name="lName" /></td></tr>
-<tr><td>Email</td><td><input type="text" name="email" /></td></tr>
-<tr><td>Phone</td><td><input type="text" name="phone" /></td></tr>
-<tr><td>StreetAddress1</td><td><input type="text" name="StreetAddress1" /></td></tr>
-<tr><td>StreetAddress2</td><td><input type="text" name="StreetAddress2" /></td></tr>
-<tr><td>City</td><td><input type="text" name="City" /></td></tr>
+    <tr>
+	<td>
+	    Unique ID
+	</td>
+	<td >
+	    <input type="text" name="ID" disabled
+		<?PHP
+		    if(!isset($conID))
+		    {
+			if(isset($ID))
+			{
+			    echo 'value='.$ID;
+			}
+		    }
+		    else
+		    {
+			echo 'value='.$conID;
+		    }
+		?>
+	    />
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    First Name
+	</td>
+	<td>
+	    <input type="text" name="fName" 
+		<?PHP
+		    if(isset($fName))
+		    {
+			echo 'value="'.$fName.'"';
+		    }
+		?>
+	    />
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    Last Name
+	</td>
+	<td>
+	    <input type="text" name="lName" 
+		<?PHP
+		    if(isset($lName))
+		    {
+			echo 'value="'.$lName.'"';
+		    }
+		?>
+	    />
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    Email
+	</td>
+	<td>
+	    <input type="text" name="email" 
+		<?PHP
+		    if(isset($email))
+		    {
+			echo 'value="'.$email.'"';
+		    }
+		?>	    
+	    />
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    Phone
+	</td>
+	<td>
+	    <input type="text" name="phone" 
+		<?PHP
+		    if(isset($phone))
+		    {
+			echo 'value="'.$phone.'"';
+		    }
+		?>	   	   
+	    />
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    StreetAddress1
+	</td>
+	<td>
+	    <input type="text" name="StreetAddress1" 
+		<?PHP
+		    if(isset($StreetAddress1))
+		    {
+			echo 'value="'.$StreetAddress1.'"';
+		    }
+		?>	   	   	    
+	    />
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    StreetAddress2
+	</td>
+	<td>
+	    <input type="text" name="StreetAddress2"
+		<?PHP
+		    if(isset($StreetAddress2))
+		    {
+			echo 'value="'.$StreetAddress2.'"';
+		    }
+		?>	    
+	    />
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    City
+	</td>
+	<td>
+	    <input type="text" name="City" 
+		<?PHP
+		    if(isset($City))
+		    {
+			echo 'value="'.$City.'"';
+		    }
+		?>	    
+	    />
+	</td>
+    </tr>
 
-<tr><td>State</td><td><select name="State">
-	<option value="AL">Alabama</option>
-	<option value="AK">Alaska</option>
-	<option value="AZ">Arizona</option>
-	<option value="AR">Arkansas</option>
-	<option value="CA">California</option>
-	<option value="CO">Colorado</option>
-	<option value="CT">Connecticut</option>
-	<option value="DE">Delaware</option>
-	<option value="DC">District of Columbia</option>
-	<option value="FL">Florida</option>
-	<option value="GA">Georgia</option>
-	<option value="HI">Hawaii</option>
-	<option value="ID">Idaho</option>
-	<option value="IL">Illinois</option>
-	<option value="IN">Indiana</option>
-	<option value="IA">Iowa</option>
-	<option value="KS">Kansas</option>
-	<option value="KY">Kentucky</option>
-	<option value="LA">Louisiana</option>
-	<option value="ME">Maine</option>
-	<option value="MD">Maryland</option>
-	<option value="MA">Massachusetts</option>
-	<option value="MI">Michigan</option>
-	<option value="MN">Minnesota</option>
-	<option value="MS">Mississippi</option>
-	<option value="MO">Missouri</option>
-	<option value="MT">Montana</option>
-	<option value="NE">Nebraska</option>
-	<option value="NV">Nevada</option>
-	<option value="NH">New Hampshire</option>
-	<option value="NJ">New Jersey</option>
-	<option value="NM">New Mexico</option>
-	<option value="NY">New York</option>
-	<option value="NC">North Carolina</option>
-	<option value="ND">North Dakota</option>
-	<option value="OH">Ohio</option>
-	<option value="OK">Oklahoma</option>
-	<option value="OR">Oregon</option>
-	<option value="PA">Pennsylvania</option>
-	<option value="RI">Rhode Island</option>
-	<option value="SC">South Carolina</option>
-	<option value="SD">South Dakota</option>
-	<option value="TN">Tennessee</option>
-	<option value="TX">Texas</option>
-	<option value="UT">Utah</option>
-	<option value="VT">Vermont</option>
-	<option value="VA">Virginia</option>
-	<option value="WA">Washington</option>
-	<option value="WV">West Virginia</option>
-	<option value="WI">Wisconsin</option>
-	<option value="WY">Wyoming</option>
-</select></td></tr>
-<tr><td>PostalCode</td><td><input type="text" name="PostalCode" /></td></tr>
-<tr><td>Country</td><td><input type="text" name="Country" value="United States"/></td></tr>
-<tr height='60'><td></td></tr>
+    <tr>
+	<td>
+	    State
+	</td>
+	<td>
+	    <select name="State">
+		<?PHP
+		    if(isset($State))
+		    {
+			echo '<option value="'.$State.'" selected="selected">'.$State.'</option>';
+		    }
+		?>	    
+		<option value="AL">Alabama</option>
+		<option value="AK">Alaska</option>
+		<option value="AZ">Arizona</option>
+		<option value="AR">Arkansas</option>
+		<option value="CA">California</option>
+		<option value="CO">Colorado</option>
+		<option value="CT">Connecticut</option>
+		<option value="DE">Delaware</option>
+		<option value="DC">District of Columbia</option>
+		<option value="FL">Florida</option>
+		<option value="GA">Georgia</option>
+		<option value="HI">Hawaii</option>
+		<option value="ID">Idaho</option>
+		<option value="IL">Illinois</option>
+		<option value="IN">Indiana</option>
+		<option value="IA">Iowa</option>
+		<option value="KS">Kansas</option>
+		<option value="KY">Kentucky</option>
+		<option value="LA">Louisiana</option>
+		<option value="ME">Maine</option>
+		<option value="MD">Maryland</option>
+		<option value="MA">Massachusetts</option>
+		<option value="MI">Michigan</option>
+		<option value="MN">Minnesota</option>
+		<option value="MS">Mississippi</option>
+		<option value="MO">Missouri</option>
+		<option value="MT">Montana</option>
+		<option value="NE">Nebraska</option>
+		<option value="NV">Nevada</option>
+		<option value="NH">New Hampshire</option>
+		<option value="NJ">New Jersey</option>
+		<option value="NM">New Mexico</option>
+		<option value="NY">New York</option>
+		<option value="NC">North Carolina</option>
+		<option value="ND">North Dakota</option>
+		<option value="OH">Ohio</option>
+		<option value="OK">Oklahoma</option>
+		<option value="OR">Oregon</option>
+		<option value="PA">Pennsylvania</option>
+		<option value="RI">Rhode Island</option>
+		<option value="SC">South Carolina</option>
+		<option value="SD">South Dakota</option>
+		<option value="TN">Tennessee</option>
+		<option value="TX">Texas</option>
+		<option value="UT">Utah</option>
+		<option value="VT">Vermont</option>
+		<option value="VA">Virginia</option>
+		<option value="WA">Washington</option>
+		<option value="WV">West Virginia</option>
+		<option value="WI">Wisconsin</option>
+		<option value="WY">Wyoming</option>
+	    </select>
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    PostalCode
+	</td>
+	<td>
+	    <input type="text" name="PostalCode" 
+		<?PHP
+		    if(isset($PostalCode))
+		    {
+			echo 'value="'.$PostalCode.'"';
+		    }
+		?>	    
+	    />
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    Country
+	</td>
+	<td>
+	    <input type="text" name="Country"
+		<?PHP
+		    if(isset($Country))
+		    {
+			echo 'value="'.$Country.'"';
+		    }
+		    else
+		    {
+			echo 'value="United States"';			
+		    }
+		    
+		?>	 
+	    />
+	</td>
+    </tr>
+    <tr height='60'>
+	<td>
+	</td>
+    </tr>
 </table>
 <table>
-<tr><td>Next step for Client:</td></tr>
-<tr><td><input type="checkbox" name="207" /> Send Print Kit</td><td><input type=checkbox name="213" /> Complimentary Session</td><td><input type=checkbox name="215" /> FHA With Paypal Request</td><td><input type=checkbox name="219" /> Fingerprint With Paypal Request</td></tr>
+    <tr>
+	<td>
+	    Next step for Client:
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    <input type="checkbox" name="207"
+	    
+	    />
+		Send Print Kit
+	</td>
+	<td>
+	    <input type=checkbox name="213"
+	    />
+		Complimentary Session
+	</td>
+	<td>
+	    <input type=checkbox name="215"
+	    
+	    />
+		FHA With Paypal Request
+	</td>
+	<td>
+	    <input type=checkbox name="219"
+		
+	    />
+		Fingerprint With Paypal Request
+	</td>
+    </tr>
 <tr><td><input type="checkbox" name="209" /> Send Gift</td><td><input type=checkbox name="news2" /> Coaching Session</td><td><input type=checkbox name="217" /> FHA Info Only</td><td><input type=checkbox name="221" /> Fingerprint Info Only</td></tr>
 <tr><td><input type="checkbox" name="135" /> Send Thank you card</td><td><input type=checkbox name="news2" /> HA Intensive</td><td><input type=checkbox name="news2" /> Family Analysis</td><td><input type=checkbox name="news2" /> Long Term Follow up</td></tr>
 <tr><td><input type="checkbox" name="187" /> Print Kit - Sent</td><td><input type=checkbox name="news2" /> HA Yearlong</td><td><input type=checkbox name="news2" /> Couples Analysis</td><td><input type=checkbox name="news2" /> Do not re-contact</td></tr>
--- Samples/New API Samples/PHP/posttovars.php
+++ Samples/New API Samples/PHP/posttovars.php
@@ -18,18 +18,18 @@ if (empty($phone)) unset($phone);
 if (isset($_POST['StreetAddress1'])) $StreetAddress1 = $_POST['StreetAddress1'];
 if (empty($StreetAddress1)) unset($StreetAddress1);
 
-if (isset($_POST['fName'])) $fName = $_POST['fName'];
-if (empty($fName)) unset($fName);
+if (isset($_POST['StreetAddress2'])) $StreetAddress2 = $_POST['StreetAddress2'];
+if (empty($StreetAddress2)) unset($StreetAddress2);
 
-if (isset($_POST['fName'])) $fName = $_POST['fName'];
-if (empty($fName)) unset($fName);
+if (isset($_POST['City'])) $City = $_POST['City'];
+if (empty($City)) unset($City);
 
-if (isset($_POST['fName'])) $fName = $_POST['fName'];
-if (empty($fName)) unset($fName);
+if (isset($_POST['State'])) $State = $_POST['State'];
+if (empty($State)) unset($State);
 
-if (isset($_POST['fName'])) $fName = $_POST['fName'];
-if (empty($fName)) unset($fName);
+if (isset($_POST['PostalCode'])) $PostalCode = $_POST['PostalCode'];
+if (empty($PostalCode)) unset($PostalCode);
 
-if (isset($_POST['fName'])) $fName = $_POST['fName'];
-if (empty($fName)) unset($fName);
+if (isset($_POST['Country'])) $Country = $_POST['Country'];
+if (empty($Country)) unset($Country);
 ?>
